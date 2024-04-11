8632b7e7dd2fc488a78d827b5258630d571e61f5
babbaja <cho00070265@gmail.com>
2024-04-11 11:21:39 +0900

    first commit

---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,48 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // @Autowired private final MemberService memberService; 필드 주입
+
+    @Autowired
+    public MemberController(MemberService memberService) { //생성자 주입
+        this.memberService = memberService;
+    }
+
+    //setter 주입
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,67 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void BeforeEach() { // DI 라고한다. 관계 주입?
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        //given 이런 상황이 주어짐
+        Member member = new Member();
+        member.setName("hello");
+
+        //when 이걸로 실행했을때
+        Long saveId = memberService.join(member);
+
+        //then 결과가 이게 나와야됨
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+        //then
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
